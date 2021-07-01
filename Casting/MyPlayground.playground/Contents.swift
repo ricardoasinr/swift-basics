416a741e3fb8380ee515917a637c88dfb94301ad
Ricardo Asin <71453150+ricardoasinr@users.noreply.github.com>
2021-07-01 00:04:45 -0400

    Casting

---
+++ Casting/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Foundation
+
+//Casting
+let num16 : UInt16 = 25
+let num8 : UInt8 = 34
+let sum = num16 + UInt16(num8)
+
+let num = 25
+let dec = 0.34
+let sum1 = Double(num) + dec
+
+let PI :Double = 3.14159
+round(PI);
+print("Deberia redondearse \(PI)");
