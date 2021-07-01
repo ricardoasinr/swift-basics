5d2ee0f3fe201e15f02acaf94f128ab7dff5fbdf
Ricardo Asin <71453150+ricardoasinr@users.noreply.github.com>
2021-06-30 23:44:03 -0400

    Other ways to write numbers
    Different ways to write numbers with decimals

--- Decimals/FloatsDoubles.playground/Contents.swift
+++ Decimals/FloatsDoubles.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 var str = UInt8.max
 str = 56
+//Cannot be a negative
 
 var pi: Float = 3.14159265
 //Float only 6 decimals
@@ -17,4 +18,12 @@ var octal = 0o45
 
 //Hexa
 var hexa = 0x11
+
+var exponent : Double = 148e3
+
+var paddedDouble = 0000123.32
+let oneMillion = 1_000_000.000_1
+
+
+
 
