5ea8bebe495418cde79969f7564e987e799a5633
Ricardo Asin <71453150+ricardoasinr@users.noreply.github.com>
2021-06-30 23:13:05 -0400

    Types of integer variables
     numbers amount that store certain types of integer variables.
    In this case we show the example of variables from 8 to 64 bits.

---
+++ Integers/IntsBits.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let age = 20
+
+let minValue8 = UInt8.min
+let maxValue8 = UInt8.max
+//Max = 255
+
+
+let minValue16 = UInt16.min
+let maxValue16 = UInt16.max
+//Max 65535
+
+let minValue32 = UInt32.min
+let maxValue32 = UInt32.max
+//Max 4294967295
+
+let minValue64 = UInt64.min
+let maxValue64 = UInt64.max
+//Max 18446744073709551615
