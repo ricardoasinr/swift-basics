ff4e1527cd2d7eae8a75b386ef3fd7f7326e2fa2
Ricardo Asin <71453150+ricardoasinr@users.noreply.github.com>
2021-07-03 22:58:33 -0400

    Tuplas

---
+++ Tuplas/Tuplas.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Playground"
+var num = 4214
+
+let tupla1 = (num, str)
+let (code, mess) = tupla1
+print(code,mess)
+
+let guardarNombres = (nombre: "Ricardo ", apellido: "Asin")
+
+print("Hola \(guardarNombres.nombre), tu apellido es \(guardarNombres.apellido)")
+
+
+
+
